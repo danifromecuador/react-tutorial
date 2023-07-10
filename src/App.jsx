7dbf509f23bfc4d89eb7bdf0ba38562b1f406c53
dani <danifromecuador@gmail.com>
2023-07-10 12:27:22 -0500

    Create empty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-
+import TodoApp from '@/components/TodoApp';
 function App() {
   return (
     <div className='app-container'>
-      hello world
+      <TodoApp />
     </div>
   );
 }
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default TodoApp;
